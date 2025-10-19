6e018716ae42588b04b9e70f9e08428503961f76
Brian McNaboe <bmcnaboe@gmail.com>
2025-10-19 10:37:01 -0400

    Introduce boot_support.py for boot logic and refactor boot.py to comply with CircuitPython requirements
    - Created boot_support.py to encapsulate all boot logic, including storage configuration, firmware update checks, and reset strategies, compiled to bytecode for efficiency.
    - Refactored boot.py to serve as a non-compiled entry point, ensuring compliance with CircuitPython's requirements while delegating boot operations to boot_support.py.
    - Enhanced structure and organization of boot processes, improving maintainability and clarity of the codebase.

--- src/code.py
+++ src/code.py
@@ -1,5 +1,5 @@
 # CircuitPython requires "code.py" to be a non-compiled (source) Python file.
 # This file cannot be replaced by a compiled .mpy file, otherwise the board will not boot your code.
 
-import base
-base.main() # assuming your app has a run() function
+import code_support
+code_support.main() # assuming your app has a run() function
